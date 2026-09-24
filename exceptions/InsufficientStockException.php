877b2c2d42d7e9ef2f892fa6faaaaed62f77342c
Emmanuel Capulong <emmanskie0314@gmail.com>
2026-09-24 11:55:37 +0800

    Keep assets folder only

--- exceptions/InsufficientStockException.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class InsufficientStockException extends Exception
-{
-}
--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-</main>
-<footer class="site-footer">
-  <p>Homestyle Filipino meals, made to order.</p>
-</footer>
-</body>
-</html>
